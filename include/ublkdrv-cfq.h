28a4486193ead2a45ef23064cbaf3e82c89486f0
Denis Pronin <dannftk@yandex.ru>
2024-06-16 09:57:08 +0300

    optimize ring-like queue to be spsc because spmc is not required
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- include/ublkdrv-cfq.h
+++ include/ublkdrv-cfq.h
@@ -12,30 +12,29 @@
 
 static inline bool ublkdrv_cfq_ack_pop(struct ublkdrv_cmd_ack const cmds[], u8 cmds_len, u8* phead, u8 const* ptail, struct ublkdrv_cmd_ack* cmd_out)
 {
-    u8 ph = smp_load_acquire(phead);
-    u8 ph_old;
-    do {
-        u8 const pt = smp_load_acquire(ptail);
-        ph_old      = ph;
-        if (unlikely(pt == ph))
-            return false;
-        BUG_ON(!(ph < cmds_len));
-        *cmd_out = cmds[ph];
-    } while (ph_old != (ph = cmpxchg_release(phead, ph, (ph + 1) % cmds_len)));
+    u8 const ch = smp_load_acquire(phead);
+    u8 const ct = smp_load_acquire(ptail);
+    if (likely(ct != ch)) {
+        BUG_ON(!(ch < cmds_len));
+        *cmd_out = cmds[ch];
+        smp_store_release(phead, (ch + 1) % cmds_len);
+        return true;
+    }
 
-    return true;
+    return false;
 }
 
 static inline bool ublkdrv_cfq_push(struct ublkdrv_cmd cmds[], u8 cmds_len, u8 const* phead, u8* ptail, struct ublkdrv_cmd const* cmd_in)
 {
-    __u8 const pt  = *ptail;
-    __u8 const npt = (pt + 1) % cmds_len;
-    if (unlikely(npt == smp_load_acquire(phead)))
-        return false;
-    BUG_ON(!(pt < cmds_len));
-    cmds[pt] = *cmd_in;
-    smp_store_release(ptail, npt);
-    return true;
+    __u8 const ct  = *ptail;
+    __u8 const nct = (ct + 1) % cmds_len;
+    if (likely(nct != smp_load_acquire(phead))) {
+        BUG_ON(!(ct < cmds_len));
+        cmds[ct] = *cmd_in;
+        smp_store_release(ptail, nct);
+        return true;
+    }
+    return false;
 }
 
 #endif /* UBLKDRV_CFQ_H */
